694ca43809138b8c82ba9cc36f2b2b14ff14e0aa
cadmy <cadmy@ya.ru>
2023-01-13 22:18:40 +0200

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,16 +1,19 @@
 #!/bin/bash
-cd /home/twohearts/two-hearts;
+local FOLDER="/home/project"
+local JAR="project.jar"
+local NAME="project"
+cd $FOLDER;
 git fetch;
 if [ $(git rev-parse HEAD) != $(git rev-parse @{u}) ]; then
         echo "Updating branch";
         git pull --no-edit;
 	mvn clean install;
-	sudo kill -9 $(jps | grep twohearts | awk '{print $1}');
-	nohup java -jar ./target/twohearts.jar;
+	sudo kill -9 $(jps | grep $NAME | awk '{print $1}');
+	nohup java -jar ./target/$JAR;
 else
         echo "No updates available";
-	if [ -z $(jps | grep twohearts) ]; then
+	if [ -z $(jps | grep  $NAME) ]; then
 		echo "No application running. Starting application"
-		nohup java -jar ./target/twohearts.jar
+		nohup java -jar ./target/$JAR
 	fi
 fi
